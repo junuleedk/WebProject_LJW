bbe09ad9e3ec45e92148f863296fa65e5e834011
junuleedk <junulee.dk@gmail.com>
2024-03-19 12:54:49 +0900

    최종본- 추가기능X, 리액트부분 수정필요, 게시판 게시글 부분 뷰컨토롤러

--- src/main/java/common/DBConnPool.java
+++ src/main/java/common/DBConnPool.java
@@ -43,7 +43,7 @@ public class DBConnPool {
 			 	얻어온다. 즉, DB연결을 위한 정보를 얻어온다. 
 			 */
 			DataSource source = 
-					(DataSource) ctx.lookup("dbcp_myoracle");
+					(DataSource) ctx.lookup("dbcp_myoracle99");
 			/*
 			 4. 커넥션풀에 생성되어 있는 객체를 가져다가 사용한다.  
 			 */
